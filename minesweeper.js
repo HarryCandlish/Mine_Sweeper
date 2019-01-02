8ae16f51768dd91f5a66cc27a821d83179fec601
HarryCandlish <hcnd90@gmail.com>
2019-01-03 09:52:27 +1300

    Minesweeper

--- minesweeper.js
+++ minesweeper.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-// Define your `board` object here!
-var board = {
-   cells: generateCells(5)
- }
+
+var board = {}
 //     {
 //       row: 0,
 //       col: 0,
@@ -39,6 +37,7 @@ function generateCells (size) {
   //return a cells array
   var cells = []
 
+
   for (i = 0; i < size; i++ ) {
 
     for (j = 0; j < size; j++){
@@ -57,15 +56,24 @@ function generateCells (size) {
 }
 
 function startGame () {
+  board['cells'] =  generateCells(3)
   // Don't remove this function call: it makes the game work!
-board.cells[4].isMine = true
+  for(x = 0; x < board.cells.length; x++){
+    var randomNum = Math.round(Math.random())
 
-for (i = 0; i < board.cells.length; i++) {
-  board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
-}
+    if (randomNum == 1) {
+      board.cells[x] .isMine = true
+    }
+  }
 
+  for (i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  }
 
-  lib.initBoard()
+    document.getElementsByClassName('board')[0].addEventListener('click', checkForWin)
+    document.getElementsByClassName('board')[0].addEventListener('contextmenu', checkForWin)
+    lib.initBoard()
+
 }
 
 // Define this function to look for a win condition:
@@ -74,40 +82,50 @@ for (i = 0; i < board.cells.length; i++) {
 // 2. Are all of the mines marked?
 
 
-function checkForWin (){}
+function checkForWin() {
 //
 //
 //   // Once [ c ] is equal to visibleNonMines the user wins.
 //   // Create an if statement for if they are equal console log this, if this is not true console.log try again.
 //
-//
-//   if (countVisibleNonMines = visibleNonMines ) {
-//     	 console.log (lib.displayMessage('You win!’))
-//     	 }
-//
-//   if else ( countVisibleNonMines = !visibleNonMines ) {
-//     	console.log(“ Try Again ”)
-//     	}
-//
-//     }
+ var totalMines = countTotalMines()
+ var markedMines = isMarked()
+  var visibleNonMines = countVisibleNonMines()
+  var totalNonMines = countTotalNonMines()
+
+    if (visibleNonMines === totalNonMines || markedMines === totalMines) {
+      	 console.log (lib.displayMessage('You win!'))
+     }
+
+    else if ( visibleNonMines != totalNonMines ) {
+      	console.log('Try Again')
+    }
+
+    }
 //
 //
 //   // Count through the array and count all of the mines and mark it [ a ]
 //   // Then, [ a ] Mines - all cells on the board  = visibleNonMines
 //
-// Function visibleNonMines (cell) {
-//
-// Var mine = 0
-//
-//   For (a =0; a < board.cells.length; a++){
-//     If ( cell[a].isMine ){
-//   	   mine ++
-//   	}
-//   }
-//
-// return mine - cells
-//
-//   }
+function countTotalNonMines () {
+  var count = 0;
+  for (var i = 0; i < board.cells.length; i++){
+    if (board.cells[i].isMine === false){
+      count++
+    }
+  }
+    return count
+}
+
+function countTotalMines () {
+  var count = 0;
+  for (var i = 0; i < board.cells.length; i++){
+    if (board.cells[i].isMine === true){
+      count++
+    }
+  }
+    return count
+}
 //
 //
 //
@@ -116,18 +134,26 @@ function checkForWin (){}
 //
 //
 //
-// Function countVisibleNonMines (cell) {
-//
-//   Var count = 0
-//
-//     For (b =0; b < board.cells.length; b++){
-//   	   if ( cell[b].!hidden .!isMine){
-//   		count++
-//   	}
-//   }
-//
-// Return count
-// }
+
+function countVisibleNonMines () {
+
+  var notMines = []
+  var count = 0
+
+  for (b =0; b < board.cells.length; b++){
+     if (  board.cells[b].isMine === false){
+       notMines.push(board.cells[b])
+     }
+  }
+
+  for( var i = 0;  i < notMines.length; i++) {
+    if (  notMines[i].hidden === false){
+      count++
+    }
+  }
+
+  return count
+}
 
 
 function countSurroundingMines (cell) {
@@ -148,3 +174,19 @@ for (j = 0; j < surrounding.length; j++) {
 
 return count
 }
+
+function isMarked () {
+  var mines = []
+  var count = 0;
+  for(i = 0; i < board.cells.length; i++){
+    if (board.cells[i].isMine){
+      mines.push(board.cells[i])
+    }
+  }
+  for(x = 0; x < mines.length; x++){
+    if (mines[x].isMarked){
+      count++
+    }
+  }
+  return count
+  }
